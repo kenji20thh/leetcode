c0c29601e990b33ddea1c8fcd74726b9aeab830b
kenji20thh <amineelayachi22@outlook.com>
2026-08-25 04:23:53 +0200

    pp

---
+++ find-missing-elements.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findMissingElements = function(nums) {
+  let result = []
+  nums.sort()
+  const start = nums[0]
+  const end = nums[nums.length - 1]
+  for (let i = start ; i < end ; i ++) {
+    if (!nums.includes(i)) result.push(i)
+    continue 
+  }
+  return result
+};

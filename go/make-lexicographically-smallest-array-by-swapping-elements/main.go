19e26ee65746e5e15b36958f19e7ef28aef328e2
kenji20thh <amineelayachi322@gmail.com>
2026-08-29 16:15:29 +0100

    pp

--- go/make-lexicographically-smallest-array-by-swapping-elements/main.go
+++ go/make-lexicographically-smallest-array-by-swapping-elements/main.go
@@ -1,16 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
 func lexicographicallySmallestArray(nums []int, limit int) []int {
-	for limit > 0 {
+	gotIt := false
+	for !gotIt {
 		for i := 0; i < len(nums)-1; i++ {
-			if nums[i] > nums[i+1] {
-				nums[i], nums[i+1] = nums[i+1], nums[i]
-				limit--
-				if limit == 0 {
-					break
-				}
+			min := slices.Min(nums[i : ])
+			minIndex := slices.Index(nums[i:], min) + i
+			if minIndex != i && minIndex-i <= limit && nums[i] - min < limit {
+				nums[i], nums[minIndex] = nums[minIndex], nums[i]
 			}
-		}
 	}
 	return nums
+}
+
+func main() {
+	nums := []int{1, 7, 6, 18, 2, 1}
+	limit := 3
+	result := lexicographicallySmallestArray(nums, limit)
+	fmt.Println(result)
 }

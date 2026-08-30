768a64b651dbaeee5339f696abf5ba531b3ed0c3
kenji20thh <amineelayachi322@gmail.com>
2026-08-30 09:30:14 +0100

    pp

--- removing-minimum-and-maximum-from-array.js
+++ removing-minimum-and-maximum-from-array.js
@@ -7,9 +7,17 @@ var minimumDeletions = function(nums) {
     const max = Math.max(...nums);
     const minIndex = nums.indexOf(min);
     const maxIndex = nums.indexOf(max);
-   if (minIndex > maxIndex) {
-       return nums.length - minIndex + maxIndex + 1;  
-   }
-   return nums.length - maxIndex + minIndex + 1;
+    console.log(minIndex, maxIndex);
+   if (minIndex < nums.length / 2&& maxIndex < nums.length / 2) {
+        return Math.max(minIndex, maxIndex) + 1;
+    } else if (minIndex >= nums.length / 2 && maxIndex >= nums.length / 2) {
+        return Math.max(nums.length - minIndex, nums.length - maxIndex);
+    } else {
+        return Math.min(minIndex + 1 + nums.length - maxIndex, maxIndex + 1 + nums.length - minIndex);
+    }
 
 };
+
+const nums = [2,10,7,5,4,1,8,6]
+
+console.log(minimumDeletions(nums));
